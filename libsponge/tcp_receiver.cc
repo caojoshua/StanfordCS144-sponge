4553300bd911f0f2ebf7d1845d7e7dd8cdfb2763
caojoshua <cao.joshua@yahoo.com>
2021-01-24 20:23:43 -0800

    Overall logic of TCPReceiver segment_received.
    Still need unwrap seqnos.

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,5 +1,7 @@
 #include "tcp_receiver.hh"
 
+#include <iostream>
+
 // Dummy implementation of a TCP receiver
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -12,20 +14,34 @@ using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
     const TCPHeader header = seg.header();
-    uint32_t first_seqno = header.seqno.raw_value();
+    uint32_t seqno = header.seqno.raw_value();
 
-    if (state == LISTEN && header.syn) {
-        state = SYN_RECV;
-        _ackno = WrappingInt32{first_seqno + 1};
+    // If SYN has not been received, check the header for SYN, and set the approriate member
+    // variables. Return if header does not contain SYN.
+    if (!_syn_recv) {
+        if (header.syn) {
+            _syn_recv = true;
+            _isn = seqno;
+            ++_ack_bytes; // Acknowledge the SYN byte.
+            ++seqno; // Skip SYN byte, since its not a data byte.
+        }
+        else
+          return;
     }
 
-    _reassembler.push_substring(seg.payload().copy(), first_seqno, header.fin);
-    if (header.fin)
-      state = FIN_RECV;
+    // Push the data to the reassembler.
+    std::string data = seg.payload().copy();
+    uint32_t reassembler_index = seqno - _isn - 1;
+    _reassembler.push_substring(data, reassembler_index, header.fin);
+    _ack_bytes += data.size();
+
+    // Acknowledge FIN byte.
+    if (stream_out().input_ended())
+      ++_ack_bytes;
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const {
-    return _ackno;
+    return _syn_recv ? optional<WrappingInt32>{_isn + _ack_bytes} : optional<WrappingInt32>{};
 }
 
 size_t TCPReceiver::window_size() const {
