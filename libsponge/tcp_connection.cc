e69220c44dce21fd443aa41111945e8ddc35b62e
caojoshua <cao.joshua@yahoo.com>
2021-02-01 21:37:41 -0800

    TCPConnection remaining_outbound_capacity() and unassembled_bytes()

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -67,11 +67,11 @@ void TCPConnection::send_segments() {
     }
 }
 
-size_t TCPConnection::remaining_outbound_capacity() const { return {}; }
+size_t TCPConnection::remaining_outbound_capacity() const { return _sender.stream_in().remaining_capacity(); }
 
 size_t TCPConnection::bytes_in_flight() const { return _sender.bytes_in_flight(); }
 
-size_t TCPConnection::unassembled_bytes() const { return {}; }
+size_t TCPConnection::unassembled_bytes() const { return _receiver.unassembled_bytes(); }
 
 size_t TCPConnection::time_since_last_segment_received() const { return _time_since_last_segment_received; }
 
